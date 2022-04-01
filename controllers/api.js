b0bf5fe8596e8466b94fa78dee4e650f081253ac
Namees Albayati <nameesalbayati@Nameess-MacBook-Pro.local>
2022-04-01 02:07:22 -0500

    add test route

--- controllers/api.js
+++ controllers/api.js
@@ -1,42 +1,20 @@
 const router=require('express').Router();
-const dishes = [
-    {
-      dish_name: 'French Bread with Brie Cheese',
-      description: 'French baguette with warm brie',
-    },
-    {
-      dish_name: 'Cheese Plate with Spanish Chorizo',
-      description:
-        'Manchego, Iberico, Cabrales, fig jam, grapes, pecans, and Spanich Chorizo',
-    },
-    {
-      dish_name: 'Fish Tacos',
-      description:
-        'Battered/fried fish, corn tortillas, fresh slaw with jalepenos and cilantro, pickled red onion',
-    },
-    {
-      dish_name: 'Tropical Fruit Salad',
-      description: 'Guava, papaya, pineapple, mango, and star fruit',
-    },
-    {
-      dish_name: 'Pork Gyoza',
-      description:
-        'Homemade japanese dumplings stuffed with a pork and green onion filling',
-    },
-    {
-      dish_name: 'Yebeg Tibs with Injera Bread',
-      description:
-        'Marinated lamb dish with rosemary, garlic, onion, tomato, jalapeño and the East African spice berbere',
-    },
-    {
-      dish_name: 'Cape Malay Curry',
-      description: 'Chicken and vegitable curry dish with basmati rice',
-    },
-  ];
-  
-  //get all dishes
+
+  const {User,Comment,Posting}=require('../models')
   router.get('/', async (req, res) => {
     res.render('all',dishes[1]);
   });
+  router.get('/try',(req,res)=>{
+    const a=User.findAll();
+    res.json(a)
+  })
+router.post('/try',(req,res)=>{
+  const a=User.create({
+    username:"newuser",
+    email:"newemail@email.com",
+    password:"123456789"
+  });
+  console.log(a)
+})
   
   module.exports=router;
